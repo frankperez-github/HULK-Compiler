3be7abf319b20693eeb10fda95c07d7750f418e7
dennis-duran <dennisdanielgonzalezduran@gmail.com>
2024-07-11 10:19:31 -0400

    final commit

--- main.py
+++ main.py
@@ -4,25 +4,12 @@ from pathlib import Path
 import errors
 from Lexer.Lexer_generator import Lexer
 from Parser.Parser import ParserError, LR1Parser
-from Parser.HulkParser import HulkParser
 from Grammar.Grammar import G
-from Grammar import Grammar as Gr
 from Lexer.Tokens_lexer import tokens
 from Semantic.semantic_analysis_pipeline import semantic_analysis_pipeline
 from Code_gen.code_generator import CCodeGenerator
 from cmp.evaluation import evaluate_reverse_parse
-import dill
-import os
 
-
-def save_object(automaton, file_path):
-    with open(file_path, 'wb') as file:
-        dill.dump(automaton, file)  # Cambia 'object' por 'automaton'
-
-def load_object(file_path):
-    with open(file_path, 'rb') as file:
-        loaded_object = dill.load(file)  # Cambia 'object' por 'loaded_object'
-    return loaded_object
 
 def print_error(message):
     red = "\033[31m"
@@ -31,7 +18,7 @@ def print_error(message):
 
 
 def run_pipeline(input_path: Path, output_path: Path):
-    sys.setrecursionlimit(1000000000)
+
     if not input_path.match('*.hulk'):
         raise errors.HulkIOError(errors.HulkIOError.INVALID_EXTENSION % input_path)
 
@@ -42,12 +29,6 @@ def run_pipeline(input_path: Path, output_path: Path):
         error = errors.HulkIOError(errors.HulkIOError.ERROR_READING_FILE % input_path)
         print_error(error)
         return
-    
-    #if os.path.isfile("Lexer/hulk_lexer.pkl"):
-    #    lexer=load_object("Lexer/hulk_lexer.pkl")
-    #else:
-    #    lexer= Lexer(tokens,G.EOF)  
-    #    save_object(lexer,"Lexer/hulk_lexer.pkl")
 
     lexer= Lexer(tokens,G.EOF)
 
@@ -57,19 +38,7 @@ def run_pipeline(input_path: Path, output_path: Path):
         print_error(e)
         return
 
-    
-    #parser = HulkParser(True,True)
-
-
-    #if os.path.isfile("Parser/hulk_parser_action.pkl"):
-    #    parser=load_object("Parser/hulk_parser_action.pkl")
-    #else:
-    #    parser=LR1Parser(G)    
-    #    save_object(parser,"Parser/hulk_parser_action.pkl")
-
     parser=LR1Parser(G) 
-
-
 
     try: 
         parse, operations = parser([t.token_type for t in tokens_])
